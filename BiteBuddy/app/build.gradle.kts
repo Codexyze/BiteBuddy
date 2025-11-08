c4786d885cad9466722cc55dd29887311d0a1995
Akshay Sarapure <akshaysarapure@gmail.com>
2025-11-08 12:21:36 +0530

    feat: integrate interstitial ads into Progress Tracker and related screens

--- BiteBuddy/app/build.gradle.kts
+++ BiteBuddy/app/build.gradle.kts
@@ -32,7 +32,7 @@ android {
         applicationId = "com.scrymz.bitebuddy"
         minSdk = 26
         targetSdk = 36
-        versionCode = 8
+        versionCode = 9
         versionName = "1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
