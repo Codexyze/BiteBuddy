629caa94b8163becfab2eb774e9d6c3b2ae49372
Akshay Sarapure <akshaysarapure@gmail.com>
2025-11-30 18:00:34 +0530

    feat: add project configuration files and integrate Firebase services

--- BiteBuddy/app/build.gradle.kts
+++ BiteBuddy/app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("com.google.dagger.hilt.android")
     kotlin("plugin.serialization") version "2.0.21"
     id ("kotlin-kapt")
+    alias(libs.plugins.google.gms.google.services)
 
 }
 //
@@ -72,6 +73,7 @@ dependencies {
     implementation(libs.androidx.compose.material3)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.compose.foundation)
+    implementation(libs.firebase.inappmessaging.display)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
